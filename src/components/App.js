992647d6bacdba4df35c5c4f17dcf491b61ce916
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-06-21 11:16:18 +0000

    solution4

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import './../styles/App.css';
 import PageLayout from "./PageLayout";
 
 const App = () => {
-  const header="Welcome to my website";
+  const header="Welcome to my website.";
   const children="This is the content of my website";
   const footer="&#169; 2023 My Website.All rights reserved";
   return (
